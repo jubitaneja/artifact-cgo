8c9825648237e27e1766cc433e6263b57c88b7c5
Jubi Taneja <jubi@cs.utah.edu>
2019-11-14 11:52:20 -0800

    scripts

---
+++ build_souper_precision.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "Building Souper ..."
+souper_repo=https://github.com/jubitaneja/souper.git
+souper_branch=tags/artifact
+souper_src_dir=$(pwd)/scratch/precision/souper
+souper_build_dir=$(pwd)/scratch/precision/souper/build
+build_type=Release
+
+git clone $souper_rep $souper_src_dir
+cd $souper_src_dir
+git checkout $souper_branch
+./build_deps.sh $build_type
+mkdir -p $souper_build_dir
+cd $souper_build_dir
+cmake .. -G Ninja
+ninja
+
---
+++ prepare-req.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "Install important packages ..."
+apt-get install libgmp10 ca-certificates valgrind libc6-dev libgmp-dev cmake time patch ninja-build make autoconf automake libtool golang-go python subversion re2c git gcc g++ libredis-perl
+
+echo "Install redis ..."
+redis_repo=https://github.com/antirez/redis
+redis_branch=5.0.3
+redis_srcdir=$(pwd)/scratch/tools/redis
+
+git clone $redis_repo $redis_srcdir
+cd $redis_srcdir
+git checkout $redis_branch
+export CC=cc CXX=c++
+make -j
+
+export PATH=$redis_srcdir/src:$PATH
