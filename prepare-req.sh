bd15176010ba45ebd4f858fa6b726f824f5ca66e
Zhengyang Liu <liuz@cs.utah.edu>
2019-11-15 22:31:33 +0000

    foo

--- prepare-req.sh
+++ prepare-req.sh
@@ -1,17 +1,4 @@
 #!/bin/sh
 
-echo "Install important packages ..."
-sudo apt-get install libgmp10 ca-certificates libc6-dev libgmp-dev cmake time patch ninja-build make autoconf automake libtool python subversion re2c git gcc g++ libredis-perl unzip wget
-
-echo "Install redis ..."
-redis_repo=https://github.com/antirez/redis
-redis_branch=5.0.3
-redis_srcdir=$(pwd)/scratch/tools/redis
-
-git clone $redis_repo $redis_srcdir
-cd $redis_srcdir
-git checkout $redis_branch
-export CC=cc CXX=c++
-make -j
-
-export PATH=$redis_srcdir/src:$PATH
+echo "Install prerequisites ..."
+sudo apt-get -yy install libgmp10 ca-certificates libc6-dev libgmp-dev cmake time patch ninja-build make autoconf automake libtool python subversion re2c git gcc g++ libredis-perl unzip wget redis-server
